1595580afdeef8969a05e92fd19592e830d7a2b7
Malte Schenkling <schenkling@mediamarktsaturn.com>
2019-12-02 15:05:56 +0100

    Added initial onload and onclick, base class, and some style changes

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
-//classes
+class Playground {
+  constructor (width, height) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.ctx = this.canvas.getContext("2d");
+    document.getElementById("playground").appendChild(this.canvas);
+  }
+}
 
-//class Playground
+class PokemonPlayground extends Playground {
+  constructor (width, height) {
+    super(width, height)
+    this.updateGamesBoard();
+  }
+
+  updatePlayground () {
+    //this.clearCanvas();
+    console.log("in here");
+  }
+}
 //+ instance
 //draw method
 
@@ -16,15 +34,17 @@
 //MovingCircles
 
 window.onload = function() {
-    document.getElementById("startBtn").onclick = function() {
-      if (document.getElementById("playground").getElementsByTagName("canvas").length < 1) {
-        startGame();
-        document.getElementById("startBtn").disabled = true; //inactivate button after first time use
-      }
-    };
-  
-    function startGame() {
-      let myPlayground = new Playground(400, 600); //change dimensions !!!
-    }
+  let canvas;
+  document.getElementById("startBtn").onclick = function() {      
+    startGame();
+    document.getElementById("startBtn").disabled = true; //inactivate button after first time use
   };
+  
+  function startGame() {
+    let maxDisplayWidth = screen.width;
+    let maxDisplayHeight = screen.height;
+    let myPokemonPlayground = new PokemonPlayground(maxDisplayWidth, maxDisplayHeight/5*4); //change dimensions ?!
+    
+  }
+};
 
